edd80eec59f0c1927249a553598f53a26efc93e7
Mamjunior <166949462+Mamjunior@users.noreply.github.com>
2026-06-15 11:20:46 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Base de données en mémoire
